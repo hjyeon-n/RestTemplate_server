8fd9d208c7342461664634f5a882646e3dbf2f70
hjyeon-n <hyunjy25@gmail.com>
2021-10-17 20:49:24 +0900

    Initial commit

---
+++ src/main/java/dto/User.java
@@ -0,0 +1,14 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
